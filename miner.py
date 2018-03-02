35a37ebd21a667e8609945468efec0b1b947df8c
Baptiste Pouget <baba@firemail.cc>
2018-03-02 21:42:30 +0100

    + d'options

--- miner.py
+++ miner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import tweepy
 import string
 import sys
@@ -7,28 +9,30 @@ access_token = "4915634062-Z3qzgIwBiTpZ93oN3bCnZCbroOLJsii62M7meLl"
 access_token_secret = "0vSQlb1i9piqgOzZhAOGBnXmFmwRwzx30mdfFPTOpoD01"
 
 
-if len(sys.argv) < 2:
-    print("Un argument requis")
+
+if len(sys.argv) < 4:
+    print("utilisation : " + \
+          sys.argv[0] + \
+          " requete nombre_tweets fichier.csv\n")
     sys.exit(-1)
 
 requete = sys.argv[1]
+nbtweets = int(sys.argv[2])
+nomfic = sys.argv[3]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-nbpages = 10
+fichier = open(nomfic, "a")
 
-fichier = open("minage.csv", "w")
-for i in range(1,nbpages + 1):
-    resultats = api.search(q=requete, rpp=100, since_id=i*100, lang="fr")
-    for tweet in resultats:
-        fichier.write(tweet.created_at.strftime("%d/%m/%Y-%H:%M:%S"))
+for tweet in tweepy.Cursor(api.search, q=requete).items(nbtweets):
+    fichier.write(tweet.created_at.strftime("%d/%m/%Y-%H:%M:%S"))
+    fichier.write(";")
+    fichier.write(tweet.user.screen_name)
+    for mot in str.split(tweet.text):
         fichier.write(";")
-        fichier.write(tweet.user.screen_name)
-        for mot in str.split(tweet.text):
-            fichier.write(";")
-            fichier.write(mot)
-        fichier.write("\n")
+        fichier.write(mot)
+    fichier.write("\n")
             
 fichier.close()
